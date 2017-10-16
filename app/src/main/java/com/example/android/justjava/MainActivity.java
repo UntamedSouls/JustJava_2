ffcff4556f5a9f0dbb57302054054f4b5fa7b490
UntamedSouls <585579@student.dmschools.org>
2017-10-16 10:38:14 -0500

    1st update

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -36,8 +36,10 @@ public class MainActivity extends AppCompatActivity {
          * This method is called when the order button is clicked.
          */
     public void submitOrder(View view) {
-        display(quantity);
-        displayPrice(quantity * 5);
+        String priceMessage = "Total: $" + price;
+        priceMessage = priceMessage + "\nThank you";
+        displayMessage(priceMessage);
+    }
 
     /**
      * This method displays the given quantity value on the screen.
@@ -53,5 +55,12 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
